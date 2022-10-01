d1488d48b7802f1e52cf98e36140aba2d04388f7
salma <smorsy25699@gmail.com>
2022-10-01 17:54:41 +0200

    details

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -7,7 +7,58 @@ export default function Item() {
     const { productId } = useParams();
   const thisProduct = Product.find((prod) => prod.id === productId);
   return (
-    <div className='mx-auto 'style={{"width":"85%"}}>
+    <div className='mx-auto pt-40 flex items-center justify-between 'style={{"width":"85%"}} >      
+<div className="overflow-x-auto relative shadow-md sm:rounded-lg w-5/12">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="py-3 px-6">
+                    Product name
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Color
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+              <tr class="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  Title
+              </th>
+              <td class="py-4 px-6">
+                  {thisProduct.title}
+              </td>
+          </tr>
+              <tr class="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  Width
+              </th>
+              <td class="py-4 px-6">
+                  {thisProduct.title}
+              </td>
+          </tr>
+              <tr class="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  Height
+              </th>
+              <td class="py-4 px-6">
+                  {thisProduct.title}
+              </td>
+          </tr>
+              <tr class="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  Length
+              </th>
+              <td class="py-4 px-6">
+                  {thisProduct.title}
+              </td>
+          </tr>
+        </tbody>
+    </table>
+    <div>
+      <h2 className='text-gray-400'>Price</h2>
+    </div>
+</div> 
           <Carousel
             data={thisProduct.thumbnail}
             width="300px"
