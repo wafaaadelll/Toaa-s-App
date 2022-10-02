a20fbc16b473b4e1fa4e9ea6c74a3cec3116c64e
salma <smorsy25699@gmail.com>
2022-10-02 23:54:26 +0200

    photos

--- src/Service/Product.js
+++ src/Service/Product.js
@@ -1,182 +1,363 @@
 export default [
+  {
+    id: "1",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Bags",
+    price: 4022.22,
+    img: `${require("../Components/Images/s1.jfif")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/s1.jfif")}`,
+    },
     {
-        id: "1",
-        title: "Labore eumperspiciatis quia libero",
-        category: "pags",
-        price: 4022.22,
-        img: `${require("../Components/Images/s1.jfif")}`,
-        thumbnail: [{
-          image:
-          `${require("../Components/Images/s1.jfif")}`,
-        },
-        {
-          image:
-          `${require("../Components/Images/p3.jfif")}`,
-        }]
-      },
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "2",
+    title: "Labore eumperspiciatis quia libero",
+    category: "hand made",
+    price: 4022.22,
+    img: `${require("../Components/Images/s4.jfif")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/s4.jfif")}`,
+    },
     {
-        id: "2",
-        title: "Labore eumperspiciatis quia libero",
-        category: "hand made",
-        price: 4022.22,
-        img: `${require("../Components/Images/s4.jfif")}`,
-        thumbnail: [{
-          image:
-          `${require("../Components/Images/s4.jfif")}`,
-        },
-        {
-          image:
-          `${require("../Components/Images/p3.jfif")}`,
-        }]
-      },
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "3",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Boxes",
+    price: 4022.22,
+    img: `${require("../Components/Images/s3.jfif")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/s3.jfif")}`,
+    },
     {
-        id: "3",
-        title: "Labore eumperspiciatis quia libero",
-        category: "pags",
-        price: 4022.22,
-        img: `${require("../Components/Images/s3.jfif")}`,
-        thumbnail: [{
-          image:
-          `${require("../Components/Images/s3.jfif")}`,
-        },
-        {
-          image:
-          `${require("../Components/Images/p3.jfif")}`,
-        }]
-      },
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "4",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Bags",
+    price: 4022.22,
+    img: `${require("../Components/Images/B10.jfif")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/s2.jfif")}`,
+    },
     {
-        id: "4",
-        title: "Labore eumperspiciatis quia libero",
-        category: "hand made",
-        price: 4022.22,
-        img: `${require("../Components/Images/s2.jfif")}`,
-        thumbnail: [{
-          image:
-          `${require("../Components/Images/s2.jfif")}`,
-        },
-        {
-          image:
-          `${require("../Components/Images/p3.jfif")}`,
-        }]
-      },
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "5",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Bags",
+    price: 4022.22,
+    img: `${require("../Components/Images/s2.jfif")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/p2.jfif")}`,
+    },
     {
-        id: "5",
-        title: "Labore eumperspiciatis quia libero",
-        category: "pags",
-        price: 4022.22,
-        img: `${require("../Components/Images/s2.jfif")}`,
-        thumbnail: [{
-          image:
-          `${require("../Components/Images/p2.jfif")}`,
-        },
-        {
-          image:
-          `${require("../Components/Images/p3.jfif")}`,
-        }]
-      },
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "6",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Bags",
+    price: 4022.22,
+    img: `${require("../Components/Images/B11.jfif")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/p2.jfif")}`,
+    },
     {
-        id: "6",
-        title: "Labore eumperspiciatis quia libero",
-        category: "hand made",
-        price: 4022.22,
-        img: `${require("../Components/Images/s2.jfif")}`,
-        thumbnail: [{
-          image:
-          `${require("../Components/Images/p2.jfif")}`,
-        },
-        {
-          image:
-          `${require("../Components/Images/p3.jfif")}`,
-        }]
-      },
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "7",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Bags",
+    price: 4022.22,
+    img: `${require("../Components/Images/s2.jfif")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/p2.jfif")}`,
+    },
     {
-        id: "7",
-        title: "Labore eumperspiciatis quia libero",
-        category: "pags",
-        price: 4022.22,
-        img: `${require("../Components/Images/s2.jfif")}`,
-        thumbnail: [{
-          image:
-          `${require("../Components/Images/p2.jfif")}`,
-        },
-        {
-          image:
-          `${require("../Components/Images/p3.jfif")}`,
-        }]
-      },
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "8",
+    title: "Labore eumperspiciatis quia libero",
+    category: "hand made",
+    price: 4022.22,
+    img: `${require("../Components/Images/s2.jfif")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/p2.jfif")}`,
+    },
     {
-        id: "8",
-        title: "Labore eumperspiciatis quia libero",
-        category: "hand made",
-        price: 4022.22,
-        img: `${require("../Components/Images/s2.jfif")}`,
-        thumbnail: [{
-          image:
-          `${require("../Components/Images/p2.jfif")}`,
-        },
-        {
-          image:
-          `${require("../Components/Images/p3.jfif")}`,
-        }]
-      },
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "9",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Bags",
+    price: 4022.22,
+    img: `${require("../Components/Images/s2.jfif")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/p2.jfif")}`,
+    },
     {
-        id: "9",
-        title: "Labore eumperspiciatis quia libero",
-        category: "pags",
-        price: 4022.22,
-        img: `${require("../Components/Images/s2.jfif")}`,
-        thumbnail: [{
-          image:
-          `${require("../Components/Images/p2.jfif")}`,
-        },
-        {
-          image:
-          `${require("../Components/Images/p3.jfif")}`,
-        }]
-      },
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "10",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Bags",
+    price: 4022.22,
+    width:"80%",
+    img: `${require("../Components/Images/B27.jfif")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/B16.jfif")}`,
+    },
     {
-        id: "10",
-        title: "Labore eumperspiciatis quia libero",
-        category: "pags",
-        price: 4022.22,
-        img: `${require("../Components/Images/s2.jfif")}`,
-        thumbnail: [{
-          image:
-          `${require("../Components/Images/p2.jfif")}`,
-        },
-        {
-          image:
-          `${require("../Components/Images/p3.jfif")}`,
-        }]
-      },
+      image:
+        `${require("../Components/Images/B25.jfif")}`,
+    }]
+  },
+  {
+    id: "11",
+    title: "Labore eumperspiciatis quia libero",
+    category: "hand made",
+    price: 4022.22,
+    img: `${require("../Components/Images/s2.jfif")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/p2.jfif")}`,
+    },
     {
-        id: "11",
-        title: "Labore eumperspiciatis quia libero",
-        category: "hand made",
-        price: 4022.22,
-        img: `${require("../Components/Images/s2.jfif")}`,
-        thumbnail: [{
-          image:
-          `${require("../Components/Images/p2.jfif")}`,
-        },
-        {
-          image:
-          `${require("../Components/Images/p3.jfif")}`,
-        }]
-      },
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "12",
+    title: "Labore eumperspiciatis quia libero",
+    category: "hand made",
+    price: 4022.22,
+    img: `${require("../Components/Images/s2.jfif")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/p2.jfif")}`,
+    },
     {
-        id: "12",
-        title: "Labore eumperspiciatis quia libero",
-        category: "hand made",
-        price: 4022.22,
-        img: `${require("../Components/Images/s2.jfif")}`,
-        thumbnail: [{
-          image:
-          `${require("../Components/Images/p2.jfif")}`,
-        },
-        {
-          image:
-          `${require("../Components/Images/p3.jfif")}`,
-        }]
-      },
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "13",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Macrame wall hanging",
+    price: 4022.22,
+    img: `${require("../Components/Images/h1.jpg")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/p2.jfif")}`,
+    },
+    {
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "14",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Macrame wall hanging",
+    price: 4022.22,
+    img: `${require("../Components/Images/h2.jpg")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/p2.jfif")}`,
+    },
+    {
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "15",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Back Bags",
+    price: 280,
+    img: `${require("../Components/Images/s2.jfif")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/p2.jfif")}`,
+    },
+    {
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "16",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Boxes",
+    price: 280,
+    img: `${require("../Components/Images/B1.jfif")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/B2.jfif")}`,
+    },
+    {
+      image:
+        `${require("../Components/Images/B3.jfif")}`,
+    }]
+  },
+  {
+    id: "17",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Macrame wall hanging",
+    price: 4022.22,
+    img: `${require("../Components/Images/h3.jpg")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/p2.jfif")}`,
+    },
+    {
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "18",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Macrame wall hanging",
+    price: 4022.22,
+    img: `${require("../Components/Images/h6.jpg")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/p2.jfif")}`,
+    },
+    {
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "19",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Macrame wall hanging",
+    price: 4022.22,
+    img: `${require("../Components/Images/h31.jpg")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/p2.jfif")}`,
+    },
+    {
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "20",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Bags",
+    price: 4022.22,
+    img: `${require("../Components/Images/B17.jfif")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/s1.jfif")}`,
+    },
+    {
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "21",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Bags",
+    price: 4022.22,
+    img: `${require("../Components/Images/B8.jfif")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/s1.jfif")}`,
+    },
+    {
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "22",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Bags",
+    price: 4022.22,
+    img: `${require("../Components/Images/B9.jfif")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/s1.jfif")}`,
+    },
+    {
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  {
+    id: "23",
+    title: "Labore eumperspiciatis quia libero",
+    category: "Bags",
+    price: 4022.22,
+    img: `${require("../Components/Images/B17.jfif")}`,
+    thumbnail: [{
+      image:
+        `${require("../Components/Images/s1.jfif")}`,
+    },
+    {
+      image:
+        `${require("../Components/Images/p3.jfif")}`,
+    }]
+  },
+  // {
+  //   id: "23",
+  //   title: "Labore eumperspiciatis quia libero",
+  //   category: "Bags",
+  //   price: 4022.22,
+  //   img: `${require("../Components/Images/s5.jfif")}`,
+  //   thumbnail: [{
+  //     image:
+  //       `${require("../Components/Images/s1.jfif")}`,
+  //   },
+  //   {
+  //     image:
+  //       `${require("../Components/Images/p3.jfif")}`,
+  //   }]
+  // },
 ]
