eddc70d83de69ae0ce3a8df751e2a22b4f736476
salma <smorsy25699@gmail.com>
2022-10-07 01:10:25 +0200

    About paragraph

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,15 +2,26 @@ import React from "react";
 import "./About.css";
 export default function About() {
   return (
-    <div className="pt-40">
-      <img src="" />
-      <div>
-        <p>
-          Hi , My name is Tota , I graduated from Faculty of Science , chemist
-          chemist Biology Department , Crochet has always been my passion i have
-          been working with it for 2 years , It is the only way i find myself in
-          it , Only when i am chrocheting i fell i own the world and that i can
-        </p>
+    <div style={{ width: "85%" }} className="mx-auto">
+      <div className="flex mt-40 gap-20">
+        <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png"/>
+        <div className="text-left">
+          <h3 className="font text-3xl font-bold mb-7 IconColor">WE ARE HEARTMADE FAMILY</h3>
+          <p>
+            We stablished this business 2 years ago , First it came in our mind while the
+            pandemic ( corrona virus ) started the first product was a manly
+            scarf , i could not stop using my talent in crochet i could not get
+            enough ... So my friends said why dont you start your own business?
+            <br /><br /> And the begining was at July 2020 People started to admire my
+            work and Began to make order And that was the only thing that get me
+            out from Depression , For me doing crochet isn't just a job , its a
+            life , I creat each items with love so that where the group name
+            came from ,
+            <br /><br />
+            Thanks for all people supporting my business you are not just
+            supporting business you are supporting my <strong>LIFE DREAM</strong>
+          </p>
+        </div>
       </div>
     </div>
   );
