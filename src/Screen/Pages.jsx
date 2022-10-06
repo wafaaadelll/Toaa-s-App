f62a122174be300890a1260c3a7bacb33845ef3e
salma <smorsy25699@gmail.com>
2022-10-06 23:07:17 +0200

    contact

--- src/Screen/Pages.jsx
+++ src/Screen/Pages.jsx
@@ -5,6 +5,7 @@ import Item from "../Components/Item/Item";
 import Nav from "../Components/Navbar/Nav";
 import Shop from "../Components/Shop/Shop";
 import About from "../Components/About/About";
+import Contact from "../Components/Contact/Contact";
 import Scrolling from "../scrolling";
 export default function Pages() {
   return (
@@ -17,6 +18,7 @@ export default function Pages() {
         <Route path="/Item/:productId" element={<Item />} />
         <Route path="/Shop" element={<Shop />} />
         <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
       </Routes>
       </Scrolling>
     </div>
