14234493289b4bd77ec259a5d71b3d2d88862963
Wafaa Adel <wafaaadelll99@gmail.com>
2022-11-02 00:14:03 +0200

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
               "finalcolor" : "rgb(242 242 179)",
       "color": "#eeeece",
       "head": "#d5d5b1",
-              "responsive":"#f9f1db80"
+              "responsive":"#f9f1dbab"
     },
      height: {
         100: "450px"
