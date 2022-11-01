d38b62c31d7acbc1bc7001d506a91dd9153500a1
SalmaAhmed5 <smorsy25699@gmail.com>
2022-11-02 00:31:15 +0200

    .

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -31,10 +31,10 @@ export default function Item() {
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
               <thead className="text-xs text-gray-700 uppercase bg-head dark:text-gray-400">
                 <tr>
-                  <th scope="col" className="py-3 px-6 text-black">
+                  <th scope="col" className="py-3 px-6 text-white">
                     Product name
                   </th>
-                  <th scope="col" className="py-3 px-6 text-black">
+                  <th scope="col" className="py-3 px-6 text-white">
                     Description
                   </th>
                 </tr>
